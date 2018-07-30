480ed4ea284018bb65cd6af3302d6898b5f5cb88
danielkawuma <danielkawuma@gmail.com>
2018-07-30 08:10:33 +0300

    Included a line for creating a directory for storing text files if it does not exist

--- TextScript.sh
+++ TextScript.sh
@@ -1,3 +1,9 @@
 #! /bin/bash
 
- /usr/bin/python3 /home/daniel/logReaders/TextFileExtractor.py >> /home/daniel/Renulogs/renu_`date +%Y%m%d`.txt 2>>/home/daniel/Renulogs/error.log
+DIR_TO_PUT_TEXT_FILES="/home/daniel/Renulogs"
+if [ ! -d $DIR_TO_PUT_TEXT_FILES ]; then
+  mkdir -p $DIR_TO_PUT_TEXT_FILES
+fi
+
+
+ /usr/bin/python3 /home/daniel/EmailReader/TextFileExtractor.py > /home/daniel/Renulogs/renu_`date +%Y%m%d`.txt 2>>/home/daniel/Renulogs/error.log
